6017263b8273b54cf1a3900f37a159d4064232ce
ahmadrefi <ahmadrefijuliano@gmail.com>
2020-07-09 05:15:28 +0700

    update

--- bot.py
+++ bot.py
@@ -19,16 +19,14 @@ logger = logging.getLogger(__name__)
 
 
 def getCreds():
-  # The file token.pickle stores the user's access and refresh tokens, and is
-  # created automatically when the authorization flow completes for the first
-  # time.
+
   creds = None
   SCOPES = 'https://www.googleapis.com/auth/drive'
 
   if os.path.exists('token.pickle'):
       with open('token.pickle', 'rb') as token:
           creds = pickle.load(token)
-  # If there are no (valid) credentials available, let the user log in.
+
   if not creds or not creds.valid:
       if creds and creds.expired and creds.refresh_token:
           creds.refresh(Request())
@@ -36,7 +34,7 @@ def getCreds():
           flow = InstalledAppFlow.from_client_secrets_file(
               'credentials.json', SCOPES)
           creds = flow.run_local_server(port=0)
-      # Save the credentials for the next run
+
       with open('token.pickle', 'wb') as token:
           pickle.dump(creds, token)
 
