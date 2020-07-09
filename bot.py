3b68fb6ff1c233c1c781504d950ac8c0845f626e
ahmadrefi <ahmadrefijuliano@gmail.com>
2020-07-09 07:03:56 +0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -32,7 +32,7 @@ def getCreds():
           creds.refresh(Request())
       else:
           flow = InstalledAppFlow.from_client_secrets_file(
-              'credentials.json', SCOPES)
+              'client_secrets.json', SCOPES)
           creds = flow.run_local_server(port=0)
 
       with open('token.pickle', 'wb') as token:
